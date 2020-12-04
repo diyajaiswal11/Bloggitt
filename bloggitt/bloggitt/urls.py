d4182536f640e48eb30bfe8f3cad8f17b5032034
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-12-05 00:58:10 +0530

    feat: Reset password

--- bloggitt/bloggitt/urls.py
+++ bloggitt/bloggitt/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('',include('core.urls')),
+    path('reset/',include('django.contrib.auth.urls')),
     path('',include('core.urls')),
 ]
