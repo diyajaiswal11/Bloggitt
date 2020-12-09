14b669120139120b054e31714890f3118662584b
Vrinda <vrindagupta6828@gmail.com>
2020-12-09 22:42:39 +0530

    Added comments

--- bloggitt/core/admin.py
+++ bloggitt/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, FavouritePost
+from .models import Post, FavouritePost,Comment
 # Register your models here.
 
 
 admin.site.register(Post)
 admin.site.register(FavouritePost)
+admin.site.register(Comment)
--- bloggitt/core/models.py
+++ bloggitt/core/models.py
@@ -69,3 +69,14 @@ class FavouritePost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     posts = models.ManyToManyField(Post)
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
